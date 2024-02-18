6011803bc0619fbbd1d0e6a9d02d8b206402df00
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-02-19 00:01:32 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,27 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+class FileStorage:
+    """
+    This class serializes instances to a JSON file and deserializes JSON file to instances
+    """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def delete(self, obj=None):
+        """Deletes obj from __objects if it's inside"""
+        if obj is None:
+            return
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        if key in self.__objects:
+            del self.__objects[key]
+
+    def all(self, cls=None):
+        """Returns a dictionary of all objects"""
+        if cls is None:
+            return self.__objects
+        return {k: v for k, v in self.__objects.items() if isinstance(v, cls)}
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
