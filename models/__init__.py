e560acaedee60c31d25e892ce56ca89e9c353474
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-02-24 17:29:46 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,33 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""This module initializes the storage variable"""
+
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
+classes = {
+    'BaseModel': BaseModel,
+    'User': User,
+    'Place': Place,
+    'State': State,
+    'City': City,
+    'Amenity': Amenity,
+    'Review': Review
+    }
 
-storage = FileStorage()
+from os import getenv
+
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+
 storage.reload()
