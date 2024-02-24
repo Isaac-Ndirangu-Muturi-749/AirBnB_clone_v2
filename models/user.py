1a158bd49b144b1e91dfe569b6dd901f1f0a268c
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-02-24 17:30:24 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+"""This module defines the User class."""
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """This class represents a user in the application."""
+
+    __tablename__ = 'users'
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+
+    places = relationship("Place", cascade="all, delete", back_populates="user")
+    reviews = relationship("Review", cascade="all, delete", back_populates="user")
+
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new User instance."""
+        super().__init__(*args, **kwargs)
+
