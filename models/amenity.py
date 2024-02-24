18ea01749ae9b54b64e5beef610d42a89d16133d
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-02-24 23:45:06 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,17 +1,21 @@
+#!/usr/bin/python3
+"""This module defines the Amenity class"""
+
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+import os
+storage_engine = os.environ.get("HBNB_TYPE_STORAGE")
 
 class Amenity(BaseModel, Base):
     """This class represents an amenity in the application."""
-
-    __tablename__ = 'amenities'
-
-    name = Column(String(128), nullable=False)
-
-    place_amenities = relationship("PlaceAmenity", back_populates="amenity")
-
-    def __init__(self, *args, **kwargs):
-        """Initialize a new Amenity instance."""
-        super().__init__(*args, **kwargs)
+    if (storage_engine == "db"):
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("PlaceAmenity", back_populates="amenity")
+        place_amenities = relationship(
+            "Place",
+            secondary=place_amenity, back_populates="amenities")
+    else:
+        name = ""
