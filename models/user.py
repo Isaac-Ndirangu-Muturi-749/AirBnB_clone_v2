26d801443aaffb58b6f8faed92ef79271dd99c9e
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-02-24 23:45:47 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -5,22 +5,23 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+import os
+storage_engine = os.environ.get("HBNB_TYPE_STORAGE")
+
 
 class User(BaseModel, Base):
     """This class represents a user in the application."""
+    if (storage_engine == 'db'):
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
 
-    __tablename__ = 'users'
-
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-
-    places = relationship("Place", cascade="all, delete", back_populates="user")
-    reviews = relationship("Review", cascade="all, delete", back_populates="user")
-
-
-    def __init__(self, *args, **kwargs):
-        """Initialize a new User instance."""
-        super().__init__(*args, **kwargs)
-
+        places = relationship("Place", cascade="all, delete", back_populates="user")
+        reviews = relationship("Review", cascade="all, delete", back_populates="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
