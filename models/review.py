adf14fd1388c3d37581e948e0248ff7c30608428
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-02-24 23:45:36 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -5,17 +5,22 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
+import os
+storage_engine = os.environ.get("HBNB_TYPE_STORAGE")
+
 
 class Review(BaseModel, Base):
     """This class represents a review in the application."""
+    if (storage_engine == 'db'):
+        __tablename__ = 'reviews'
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        place = relationship("Place", back_populates="reviews")
 
-    __tablename__ = 'reviews'
+    else:
+        text = ""
+        place_id = ""
+        user_id = ""
 
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-
-    def __init__(self, *args, **kwargs):
-        """Initialize a new Review instance."""
-        super().__init__(*args, **kwargs)
 
