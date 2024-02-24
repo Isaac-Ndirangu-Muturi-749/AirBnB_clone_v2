514b2b68c9922585f798530ea9b16f7dc0b411d4
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-02-24 17:30:02 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
+"""This module defines the City class"""
 
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """A class for representing City objects"""
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+    places = relationship("Place", cascade="all, delete", back_populates="city")
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new City instance."""
+        super().__init__(*args, **kwargs)
