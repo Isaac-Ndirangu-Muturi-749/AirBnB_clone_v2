e8a4b0201f73f1429b2a8498ef00d2a603569039
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-03-23 20:11:24 +0300

    Update console.py

--- console.py
+++ console.py
@@ -258,6 +258,24 @@ class HBNBCommand(cmd.Cmd):
         storage.save()
 
 
+    # def do_all(self, args):
+    #     """ Shows all objects, or all objects of a class"""
+    #     print_list = []
+
+    #     if args:
+    #         args = args.split(' ')[0]  # remove possible trailing args
+    #         if args not in HBNBCommand.classes:
+    #             print("** class doesn't exist **")
+    #             return
+    #         for k, v in storage._FileStorage__objects.items():
+    #             if k.split('.')[0] == args:
+    #                 print_list.append(str(v))
+    #     else:
+    #         for k, v in storage._FileStorage__objects.items():
+    #             print_list.append(str(v))
+
+    #     print(print_list)
+
 
     def do_all(self, args):
         """ Shows all objects, or all objects of a class"""
@@ -268,31 +286,14 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
-                if k.split('.')[0] == args:
-                    print_list.append(str(v))
+            for k, v in storage.all(cls=HBNBCommand.classes[args]).items():
+                print_list.append(str(v))
         else:
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all().items():
                 print_list.append(str(v))
 
         print(print_list)
 
-    # def do_all(self, arg):
-    #     """Prints string representations of instances"""
-    #     args = shlex.split(arg)
-    #     obj_list = []
-    #     if len(args) == 0:
-    #         obj_dict = models.storage.all()
-    #     elif args[0] in classes:
-    #         obj_dict = models.storage.all(classes[args[0]])
-    #     else:
-    #         print("** class doesn't exist **")
-    #         return False
-    #     for key in obj_dict:
-    #         obj_list.append(str(obj_dict[key]))
-    #     print("[", end="")
-    #     print(", ".join(obj_list), end="")
-    #     print("]")
 
     def help_all(self):
         """ Help information for the all command """
