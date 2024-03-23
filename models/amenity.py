5db925ac1a5085b5884affe43b4a3d1b336d53f5
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-03-23 13:23:30 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -5,19 +5,13 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-import os
-storage_engine = os.environ.get("HBNB_TYPE_STORAGE")
-
 
 class Amenity(BaseModel, Base):
     """This class represents an amenity in the application."""
     if (storage_engine == "db"):
         __tablename__ = 'amenities'
+
         name = Column(String(128), nullable=False)
-        place_amenities = relationship(
-            "PlaceAmenity", back_populates="amenity")
-        place_amenities = relationship(
-            "Place",
-            secondary=place_amenity, back_populates="amenities")
+
     else:
         name = ""
