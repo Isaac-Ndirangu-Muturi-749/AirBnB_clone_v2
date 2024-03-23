88d5b2fbc248c5b0c2c2781fe3d9cf666038af3b
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-03-23 20:11:34 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -36,10 +36,13 @@ class BaseModel:
                 self.updated_at = datetime.now()
             if kwargs.get("id", None) is None:
                 self.id = str(uuid.uuid4())
+            models.storage.new(self)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+
+            models.storage.new(self)
 
 
     def __str__(self):
@@ -54,7 +57,6 @@ class BaseModel:
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         self.updated_at = datetime.now()
-        models.storage.new(self)
         models.storage.save()
 
 
