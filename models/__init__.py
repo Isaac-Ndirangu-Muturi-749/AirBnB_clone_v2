45328b3e1dc8d55ac08689c4cdcfdecfb952977b
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-03-23 13:23:19 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,8 +3,6 @@
     instantiates the storage system, and defines dummy classes for further use
 """
 
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
 
 from models.base_model import BaseModel
 from models.city import City
@@ -14,22 +12,16 @@ from models.user import User
 from models.place import Place
 from models.amenity import Amenity
 
-from os import environ
 
-dummy_classes = {"BaseModel": BaseModel, "User": User,
-                 "Review": Review, "City": City,
-                 "State": State, "Place": Place,
-                 "Amenity": Amenity}
+import os
 
-dummy_tables = {"states": State, "cities": City,
-                "users": User, "places": Place,
-                "reviews": Review, "amenities": Amenity}
+storage_engine = os.environ.get("HBNB_TYPE_STORAGE")
 
-storage_engine = environ.get("HBNB_TYPE_STORAGE")
-
-if (storage_engine == "db"):
+if storage_type == 'db':
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()
+
+storage.reload()
