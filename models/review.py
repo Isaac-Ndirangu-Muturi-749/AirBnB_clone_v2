354eae14f26076d21392b281c36fc77d2a9292c2
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-03-23 13:23:35 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -5,18 +5,19 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
-import os
-storage_engine = os.environ.get("HBNB_TYPE_STORAGE")
-
 
 class Review(BaseModel, Base):
     """This class represents a review in the application."""
     if (storage_engine == 'db'):
         __tablename__ = 'reviews'
-        text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        place = relationship("Place", back_populates="reviews")
+
+    place_id = Column(String(60),
+                      ForeignKey('places.id', ondelete='CASCADE'),
+                      nullable=False)
+    user_id = Column(String(60),
+                     ForeignKey('users.id', ondelete='CASCADE'),
+                     nullable=False)
+    text = Column(String(1024), nullable=False)
 
     else:
         text = ""
