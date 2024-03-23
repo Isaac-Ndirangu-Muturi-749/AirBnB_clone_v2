a784778bfdd878a6ac5e577399872723ede5b97b
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-03-23 13:24:01 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -4,23 +4,27 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
-import os
-storage_engine = os.environ.get("HBNB_TYPE_STORAGE")
+from models.city import City
 
 
 class State(BaseModel, Base):
     """A class for representing State objects"""
     if storage_engine == 'db':
         __tablename__ = 'states'
+
         name = Column(String(128), nullable=False)
-        cities = relationship("City", cascade="all, delete", backref="state")
+
+        cities = relationship("City",
+                            backref="state",
+                            cascade="all, delete-orphan",
+                            passive_deletes=True)
+
     else:
         name = ""
 
         @property
         def cities(self):
             """Getter attribute that returns the list of City instances"""
-            all_cities = models.storage.all(City)
-            return [city for city in all_cities.values()
+            all_cities = models.storage.all(City).values()
+            return [city for city in all_cities
                     if city.state_id == self.id]
